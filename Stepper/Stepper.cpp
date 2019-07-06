5e39697684e14434cfd5bf7e87d154c073a11f4f
Joseph Zawisa <jazawisa@umich.edu>
2019-07-05 22:40:49 -0400

    Add constructor

--- Stepper/Stepper.cpp
+++ Stepper/Stepper.cpp
@@ -5,3 +5,5 @@
 //
 
 #include "Stepper.h"
+
+Stepper::Stepper(const int _StepsPerRevolution, const int _PinA, const int _PinB, const int _PinC, const int _PinD) : PinA(_PinA), PinB(_PinB), PinC(_PinC), PinD(_PinD), StepsPerRevolution(_StepsPerRevolution) {}
--- Stepper/Stepper.h
+++ Stepper/Stepper.h
@@ -6,4 +6,17 @@
 
 #ifndef JZ_CALIFORNIA
 #define JZ_CALIFORNIA
+
+class Stepper {
+public:
+    Stepper(const int _StepsPerRevolution, const int _PinA, const int _PinB, const int _PinC, const int _PinD);
+    
+private:
+    const int PinA, PinB; // Pins A and B are on the same coil
+    const int PinC, PinD; // Pins C and D are on the same coil
+    const int StepsPerRevolution; // Steps Per Revolution
+    
+    int current; // Current Step
+};
+
 #endif
