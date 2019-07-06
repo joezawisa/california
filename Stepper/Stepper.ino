8c2b15d82d30ccc342c49d9b4568912522aabb38
Joseph Zawisa <jazawisa@umich.edu>
2019-07-05 23:01:47 -0400

    Remove redundant code
    This code was moved to the Stepper library.

--- Stepper/Stepper.ino
+++ Stepper/Stepper.ino
@@ -1,50 +1,11 @@
-// Specify the electrical connections to the motor driver here
-#define AA 5
-#define NA 4
-#define BB 3
-#define NB 2
+#include "Stepper.h"
 
-// Global variables
-const int pause = 10;
+Stepper motor(200, 2, 3, 4, 5);
 
 void setup() {
-  // Set the output pins to drive the motor
-  pinMode(AA, OUTPUT);
-  pinMode(NA, OUTPUT);
-  pinMode(BB, OUTPUT);
-  pinMode(NB, OUTPUT);
+  // Put you setup code here
 }
 
 void loop() {
-  // Step 4
-  digitalWrite(AA, HIGH);
-  digitalWrite(NA, LOW);
-  digitalWrite(BB, LOW);
-  digitalWrite(NB, HIGH);
-  
-  delay(pause);
-
-  // Step 3
-  digitalWrite(AA, LOW);
-  digitalWrite(NA, HIGH);
-  digitalWrite(BB, LOW);
-  digitalWrite(NB, HIGH);
-
-  delay(pause);
-
-  // Step 2
-  digitalWrite(AA, LOW);
-  digitalWrite(NA, HIGH);
-  digitalWrite(BB, HIGH);
-  digitalWrite(NB, LOW);
-
-  delay(pause);
-  
-  // Step 1
-  digitalWrite(AA, HIGH);
-  digitalWrite(NA, LOW);
-  digitalWrite(BB, HIGH);
-  digitalWrite(NB, LOW);
-
-  delay(pause);
+  // Put your loop code here
 }
